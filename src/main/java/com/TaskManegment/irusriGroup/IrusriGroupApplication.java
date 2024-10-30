a5aafe69ccf6be30bc1ce3a2b326249fd579ac94
vimukthikodippili <kavimukthikodippili@email.com>
2024-10-30 20:52:47 +0530

    first commit

---
+++ src/main/java/com/TaskManegment/irusriGroup/IrusriGroupApplication.java
@@ -0,0 +1,13 @@
+package com.TaskManegment.irusriGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrusriGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrusriGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TaskManegment/irusriGroup/IrusriGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskManegment.irusriGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrusriGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
